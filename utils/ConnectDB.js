b840a44cd3cbdc60940ab7a076d36691859fb5bc
Montagab Al-Hamawy <ma0152260@gmail.com>
2023-09-21 15:27:15 +0300

    Update ConnectDB.js

--- utils/ConnectDB.js
+++ utils/ConnectDB.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 export default async function ConnectDB() {
   try {
+    // Enter the Connect URL for MongoDB 
     await mongoose.connect('mongodb://localhost:27017/fullnext',
       { useUnifiedTopology: true, useNewUrlParser: true });
 
