5c5adb6f189e1e7db6102d8acf7f06d701076965
Montagab Al-Hamawy <ma0152260@gmail.com>
2023-09-21 15:28:43 +0300

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,8 @@
 var domain;
 
 if(process.env.NODE_ENV === 'production'){
-    
+    //Enter the Domain
+    // EX: domain:'https://yourDomain.com/api/'
 }else{
     domain='http://localhost:3000/api/'
 }
